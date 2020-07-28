b595a38f20f3ae81d7e3462e9e69b420778dc0e4
HaiqaK <68754642+HaiqaK@users.noreply.github.com>
2020-07-27 20:16:33 -0400

    Update 170663680_a04.c

--- 170663680_a04.c
+++ 170663680_a04.c
@@ -4,5 +4,16 @@ Project: cp386a04
 File: 170663680_a04.c
 ------------------------------
 Author: Haiqa Khurshid
+GitHub: HaiqaK
+Repsository: https://github.com/HaiqaK/CP386_A04
 ------------------------------
 */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <semaphore.h> 
