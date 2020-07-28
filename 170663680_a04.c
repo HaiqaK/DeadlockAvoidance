667351c5616bb768c7b40cda9e42d6897d2d8239
HaiqaK <68754642+HaiqaK@users.noreply.github.com>
2020-07-28 16:14:16 -0400

    readFile funtion

--- 170663680_a04.c
+++ 170663680_a04.c
@@ -29,7 +29,7 @@ int resource;
 int *sequence;
 
 
-int readFile(char* fileName);
+int **readFile(char* fileName);
 void *threadRun(void *t);
 int *safetySeq();
 void spd(int *data, int a);         // single pointer data
@@ -37,6 +37,63 @@ void dpd(int **data, int a, int b); // double pointer dadta
 
 int **readFile(char *fileName)
 {
+        FILE *in = fopen(fileName, "r");
+    if (!in)
+    {
+        printf("Child A: Error in opening input file... exiting with error code -1\n");
+        return -1;
+    }
+
+    struct stat st;
+    fstat(fileno(in), &st);
+    char *fileContent = (char *)mallock(((int)st.st_size + 1) * sizeof(char));
+    fileContent[0] = '\0';
+    while(!feof(in))
+    {
+        char line[100];
+        if(fgets(line, 100, in) != NULL)
+        {
+            strncat(fileContent, line, strlen(line));
+        }
+    }
+    fclose(in);
+
+    char *command = NULL;
+    char *fileCopy = (char *) malloc((strlen(fileContent) + 1) * sizeof(char));
+    strcpy(fileCopy, fileContent);
+    command = strtok(fileCpy), "\r\n");
+    while(command != NULL)
+    {
+        customer++;
+        command = strtok(NULL, "\r\n");
+    }
+    strcpy(fileCopy, fileContent);
+    char *lines[customer];
+    int i = 0;
+    commmand = strtok(fileCopy, "\r\n");
+    while (command != NULL)
+    {
+        lines[i] = malloc(sizeof(command) * sizeof(char));
+        strcpy(lines[i], command);
+        i++;
+        command = strtok(NULL, "\r\n"):
+    }
+    int **maximum = malloc(sizeof(int *) * customer);
+    for (int j = 0; j < customer; j++)
+    {
+        int *temp = malloc(sizeof(int) * resource);
+        char *token = NULL;
+        int k = 0;
+        token = strtok(lines[j], ",");
+        while (token != NULL)
+        {
+            temp[k] = atoi(token);
+            k++;
+            token = strtok(NULL, ",")
+        }
+        maximum[j] = temp;
+    }
+    return maximum;
 }
 
 int main(int argc, char *argv[])
